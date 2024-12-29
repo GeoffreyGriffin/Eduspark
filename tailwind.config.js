25e2933bc17bee7a1a4691073470974c20b20363
GeoffretGriffin <griffo@DESKTOP-QM8LH4N.>
2024-12-29 19:00:27 +0300

    Fixing them errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}", // Adjust paths to match your project structure
+    "paths", {
+      "@/*": ["./src/*"] 
+    }, // Adjust paths to match your project structure
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
